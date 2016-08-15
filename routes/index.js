fc0fa38ad51dd345c08c769b9ea4460ea2e1f825
SandeepPandey <s4n989@gmail.com>
2016-08-15 13:45:30 +0530

    Added code related to returning response for recorded request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var collectionParser = require('../lib/collectionParsor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
+router.get('/collection', function(req, res, next) {
+	return res.send(collectionParser.getTwitterApiCollection().toJSON());
+});
+
+router.use('/mock', function(req, res, next) {
+	var responseVal = collectionParser.getResponse(req.url, req.method, req.query);
+	res.headers = responseVal.headers;
+	res.send(responseVal);
+});
 module.exports = router;
